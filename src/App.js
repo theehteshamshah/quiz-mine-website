aa4a6688ea450a3df592bb54d3fcd2c2d4955156
theehteshamshah <108350936+theehteshamshah@users.noreply.github.com>
2022-09-28 20:37:12 +0500

    edit register form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
 
 function App() {
   return (
     <div>
+      <Link to={/login}>Log in</Link>
       <Routes>
         <Route path="/login" element={<LoginForm />}></Route>
+        <Route path="/Register" element={<RegisterForm />}></Route>
       </Routes>
     </div>
   );
