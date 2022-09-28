1874816240ecd4c00e0bd3aae0bced4ab111ecbb
theehteshamshah <108350936+theehteshamshah@users.noreply.github.com>
2022-09-28 20:15:05 +0500

    Added fonts and icon

---
+++ src/components/LoginForm.js
@@ -0,0 +1,28 @@
+import { useState} from "react";
+
+function LoginForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('login');
+    }
+    return (
+        <form>
+            <h1>Login</h1>
+            <p>Welcome to Login </p>
+
+            <div className="input-container">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" value={password} onChange={(e) => setEmail(e.target.value)} required />
+            </div>
+
+            <button type="submit">Login</button>
+        </form>
+    )
+}
+
+export default LoginForm;
